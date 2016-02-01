f6b8652070fa0ce8ad2291c24305d684bf209272
Jiezhao Guan <lancer.rose@yahoo.com>
2016-02-01 16:52:08 +0800

    Update on ex31.py
    There are 3 syntax errors. one is at line 12.It should be elif bear =="2":,missing ":"
    The other one is at line 14.Missing ":".The last one is at line 24.Missing ":"

--- ex31.py
+++ ex31.py
@@ -9,9 +9,10 @@ if door == '1':
    bear = raw_input('$ ')
    if bear =="1":
       print "The bear eats your face off.Good job!"
-   elif bear =="2"
+   elif bear =="2":
       print "The bear eats your legs off.Good job!"
-   else print "Well,doing %s is probably better.Bear runs away." % bear
+   else:
+      print "Well,doing %s is probably better.Bear runs away." % bear
 elif door == "2":
    print "You stare into the endless abyss at Cthulhu's retina."
    print "1.Blueberries."
@@ -20,7 +21,7 @@ elif door == "2":
    
    insanity = raw_input("> ")
    
-   if insanity == "1" or insanity == "2"
+   if insanity == "1" or insanity == "2":
       print "Your body survives powered by a mind of jello.Good job!"
    else:
       print "The insanity rots your eyes into a pool of muck.Good job!"
