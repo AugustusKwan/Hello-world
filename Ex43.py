cb2431b052924012f10bd8f2452d65255f495489
Jiezhao Guan <lancer.rose@yahoo.com>
2016-02-22 16:41:32 +0800

    Update Ex43.py
    there is no intend for the line 10 exit(1)
    For the scene of the CentralCorridor(),there is the intend problem

--- Ex43.py
+++ Ex43.py
@@ -7,7 +7,7 @@ class Scene(object):
 
     def enter(self):
         print "This scene is not yet configured.Subclass it and implement enter()."
-         exit(1)
+        exit(1)
 
 #Class engine
 class Engine(object):
@@ -51,37 +51,37 @@ class CentralCorridor(Scene):
         print "flowing around his hate filled body. He's blocking the door to the"
         print "Armory and about to pull a weapon to blast you."
 
-    action = raw_input("> ")
+        action = raw_input("> ")
 
-    if action == "Shoot!" :
-        print "Quick on the draw you yank out your blaster and fire it at the Gothon."
-        print "His clown costume is flowing and moving around his body,which throws"
-        print "off your aim. Your laser hits his costume but misses him entirely.This"
-        print "makes him fly into a rage and blast you repeatedly in the face until"
-        print "you are dead. Then he eats you."
-        return 'death'
+        if action == "Shoot!" :
+           print "Quick on the draw you yank out your blaster and fire it at the Gothon."
+           print "His clown costume is flowing and moving around his body,which throws"
+           print "off your aim. Your laser hits his costume but misses him entirely.This"
+           print "makes him fly into a rage and blast you repeatedly in the face until"
+           print "you are dead. Then he eats you."
+           return 'death'
 
-    elif action == "dodge!":
-        print "Like a world class boxer you dodge,weave,slip and slide right"
-        print "as the Gothon's blaster cranks a laster past your head."
-        print "In the middle of your artful dodge your foot slips and you"
-        print "bang your head on the metal wall and pass out."
-        print "You wake up shortly after only to die as the Gothon stomps on"
-        print "your head and eats you."
-        return 'death'
+        elif action == "dodge!":
+           print "Like a world class boxer you dodge,weave,slip and slide right"
+           print "as the Gothon's blaster cranks a laster past your head."
+           print "In the middle of your artful dodge your foot slips and you"
+           print "bang your head on the metal wall and pass out."
+           print "You wake up shortly after only to die as the Gothon stomps on"
+           print "your head and eats you."
+           return 'death'
 
-    elif action == "tell a joke":
-        print "Lucky for you they made you learn Gothon insults in the academy."
-        print "You tell the one Gothon joke you know."
-        print "Lbhe zbgure vf fb sng,jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr."
-        print "The Gothon stops,tries not to laugh,then busts out laughing and can't move."
-        print "While he's laughing you run up and shoot him square in the head"
-        print "putting him down,then jump through the Weapon Armory door."
-        return 'laser_weapon_armory'
+        elif action == "tell a joke":
+           print "Lucky for you they made you learn Gothon insults in the academy."
+           print "You tell the one Gothon joke you know."
+           print "Lbhe zbgure vf fb sng,jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr."
+           print "The Gothon stops,tries not to laugh,then busts out laughing and can't move."
+           print "While he's laughing you run up and shoot him square in the head"
+           print "putting him down,then jump through the Weapon Armory door."
+           return 'laser_weapon_armory'
 
-    else:
-        print "DOES NOT COMPUTE!"
-        return 'central_corridor'
+        else:
+           print "DOES NOT COMPUTE!"
+           return 'central_corridor'
 
 #Class Laser WeaponArmory(Scene)
 class LaserWeaponArmory(Scene):
